a5678086a6552dbbd70bb73269d7c6242db603a9
MedTahaEbbou <137424508+MedTahaEbbou@users.noreply.github.com>
2023-07-03 23:27:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,47 @@
-document.getElementById('uploadForm').addEventListener('submit', function(event) {
-  event.preventDefault();
-  var videoFile = document.getElementById('videoFile').files[0];
-  
-  if (!videoFile) {
-    alert('رجاء اختر مقطعا لتتم تجزئته.');
-    return;
-  }
-  
-  var videoURL = URL.createObjectURL(videoFile);
-  splitVideo(videoURL);
-});
+function cutVideo() {
+  const videoFile = document.getElementById("videoFile").files[0];
+  const videoURL = URL.createObjectURL(videoFile);
 
-function splitVideo(videoURL) {
-  var videoElement = document.createElement('video');
-  videoElement.src = videoURL;
-  videoElement.addEventListener('loadedmetadata', function() {
-    var duration = videoElement.duration;
-    var numSegments = Math.ceil(duration / 29);
-    
-    var resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = '';
-    
-    for (var i = 0; i < numSegments; i++) {
-      var segmentStart = i * 29;
-      var segmentEnd = Math.min((i + 1) * 29, duration);
-      
-      var video = document.createElement('video');
-      video.src = videoURL + '#t=' + segmentStart + ',' + segmentEnd;
-      video.controls = true;
-      
-      var filename = 'vid' + (i + 1) + '.mp4';
-      var link = document.createElement('a');
-      link.href = video.src;
-      link.download = filename;
-      link.textContent = filename;
-      
-      var segmentDiv = document.createElement('div');
-      segmentDiv.appendChild(video);
-      segmentDiv.appendChild(link);
-      
-      resultDiv.appendChild(segmentDiv);
+  const video = document.createElement("video");
+  video.src = videoURL;
+
+  video.onloadedmetadata = function() {
+    const duration = video.duration;
+    const maxDuration = 29;
+    const numCuts = Math.ceil(duration / maxDuration);
+
+    for (let i = 0; i < numCuts; i++) {
+      const start = i * maxDuration;
+      const end = Math.min(start + maxDuration, duration);
+
+      const canvas = document.createElement("canvas");
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
+
+      const blob = dataURItoBlob(canvas.toDataURL("video/mp4"));
+      const videoBlob = new Blob([blob], { type: "video/mp4" });
+      const videoURL = URL.createObjectURL(videoBlob);
+
+      const link = document.createElement("a");
+      link.href = videoURL;
+      link.download = "vid" + (i + 1) + ".mp4";
+      link.innerHTML = "Download vid" + (i + 1) + ".mp4";
+      document.getElementById("preview").appendChild(link);
     }
-  });
+
+    URL.revokeObjectURL(videoURL);
+  };
+}
+
+function dataURItoBlob(dataURI) {
+  const byteString = atob(dataURI.split(",")[1]);
+  const mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+  const ab = new ArrayBuffer(byteString.length);
+  const ia = new Uint8Array(ab);
+  for (let i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+  return new Blob([ab], { type: mimeString });
 }
